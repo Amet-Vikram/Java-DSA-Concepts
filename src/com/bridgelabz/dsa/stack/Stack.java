fc3b0186cdc8a3c6ad71b2c7b0a6f3d45c7b56fb
Amet-Vikram <amet1197@gmail.com>
2022-03-27 22:09:31 +0530

    Stack library

--- src/com/bridgelabz/dsa/stack/Stack.java
+++ src/com/bridgelabz/dsa/stack/Stack.java
@@ -1,6 +1,6 @@
 package com.bridgelabz.dsa.stack;
 
-public class Stack<K> {
+public class Stack<K extends Comparable<K>> {
     Node<K> top;
 
     Stack(){
@@ -21,13 +21,37 @@ public class Stack<K> {
         return top == null;
     }
 
-    public <K extends Comparable<K>> K peek(){
+    public <K> K peek(){
         if (!isEmpty()) {
             return (K) top.value;
         }
         else {
             System.out.println("Stack is empty");
             return null;
+        }
+    }
+
+    public void pop(){ // remove at the beginning
+        if (top == null) {
+            System.out.print("\nStack Underflow");
+            return;
+        }
+        top = (top).next;
+    }
+
+    public void display(){
+        if (top == null) {
+            System.out.printf("\nStack Underflow");
+            System.exit(1);
+        }
+        else {
+            Node<K> temp = top;
+            while (temp != null) {
+                // print node data
+                System.out.printf("%s->", temp.value);
+                // assign temp link to temp
+                temp = temp.next;
+            }
         }
     }
 }
